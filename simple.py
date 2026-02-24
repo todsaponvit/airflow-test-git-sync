a3027625c301fedfed9f765dfa15813bc3bd533c
Todsapon Vittayasakoonchai <65076408+todsaponvit@users.noreply.github.com>
2026-02-25 01:19:55 +0700

    Rename DAG function from my_first_dag to git_sync

--- simple.py
+++ simple.py
@@ -2,10 +2,10 @@ from airflow.decorators import dag, task
 from pendulum import datetime
 
 @dag(start_date=datetime(2023, 1, 1), schedule=None, catchup=False)
-def my_first_dag():
+def git_sync():
 
     @task
     def start():
         print("Pipeline started!")
 
-start()
+git_sync()
